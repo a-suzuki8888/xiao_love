ac63d091643b7fbf44c3b056a325e90cfd50fa77
as <a.suzuki.p.8888@gmail.com>
2025-04-20 17:11:07 +0900

    魈くんのアホ毛の揺れに加え、前髪と後ろ髪も靡かせた！！！！
    可動域の塗り足しも動かしながら調整済み。

--- src/parts/js/main.js
+++ src/parts/js/main.js
@@ -3,11 +3,30 @@ fetch("parts/img/xiao.svg")
   .then(data => {
     document.getElementById("svg-container").innerHTML = data;
 
-    // SVG読み込んでからアニメ実行！
     gsap.to("#ahoge", {
       rotation: 10,
       transformOrigin: "bottom center",
-      duration: 0.5,
+      yoyo: true,
+      repeat: -1,
+      duration: 1.5,
+      ease: "sine.inOut"
+    });
+
+    gsap.to("#front_x5F_hair", {
+      rotation: 3,
+      transformOrigin: "top center",
+      yoyo: true,
+      repeat: -1,
+      duration: 2,
+      ease: "sine.inOut"
+    });
+
+    gsap.to("#back_x5F_hair", {
+      skewX: 2,
+      y: 2,
+      rotation: 1,
+      transformOrigin: "top center",
+      duration: 2,
       yoyo: true,
       repeat: -1,
       ease: "sine.inOut"
